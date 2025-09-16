7384aba096ad7c2719a53d166341d6f46b7ab9d2
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@dsn127m1249389.cfp127aluno.local>
2025-09-16 15:24:02 -0300

    style(components): navegar com código organizado e comentado

--- app/components/Navegar.tsx
+++ app/components/Navegar.tsx
@@ -1,38 +1,15 @@
 'use client' // virou um client component
 import Link from 'next/link'
 
+//Criação do type alias (apelido para uma parte que seria escrita excessivamente no código)
 type NavegarProps = {
     route: string;
 }
 
 export default function BotaoNavegar({route}: NavegarProps) {
-
-    
-
     return(
         <Link href={"/" + route}>
         <button>Ir para {route[0].toUpperCase() + route.substring(1)}</button>
         </Link>
     )
-
 }
-
-// type CardProps = {
-//     title: string;
-//     description: string;
-//   }
-  
-//   export default function Card({ title, description }: CardProps) {
-//     return (
-//          <div style={{
-//         border: "1px solid #ccc",
-//         borderRadius: "8px",
-//         padding: "1rem",
-//         margin: "1rem 0",
-//         boxShadow: "2px 2px 8px rgba(0,0,0,0.1)"
-//       }}>
-//         <h3>{title}</h3>
-//         <p>{description}</p>
-//       </div>
-//     )
-//   }
