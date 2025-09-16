92b3e01865698589f681e9132d0d091af9647fc2
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@dsn127m1249389.cfp127aluno.local>
2025-09-16 09:59:16 -0300

    chore(repo): commit inicial do projeto

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+export const metadata = {
+    title: "Meu Next App",
+    description: "Aprendendo Next.js do zero com app/"
+  }
+  
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt-BR">
+        <body>
+            <header>
+            <h1>Layout Global</h1>
+            </header>
+            <main>
+            <p>Teste estático</p>
+            </main>
+            <footer>
+            <p>Feito manualmente com Next.js</p>
+            </footer>
+        </body>
+        </html>
+    )
+}
---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+      <div>
+        <h2>Bem-vindo ao Next.js com app/</h2>
+        <p>Essa é a página inicial criada na unha 🚀</p>
+      </div>
+    )
+  }
